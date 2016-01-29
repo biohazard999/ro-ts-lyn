9026e35e803864b746ee9c46e7155e82a3ff96fe
Manuel Grundner <m.grundner@delegate.at>
2016-01-29 10:38:31 +0100

    make sure tests are running

---
+++ src/Rotslyn.Transpilers.Tests/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace Rotslyn.Transpilers.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void OneIsOne()
+        {
+            1.ShouldBe(1);
+        }
+    }
+}
